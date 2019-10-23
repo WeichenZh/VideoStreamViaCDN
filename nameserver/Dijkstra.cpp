9b70386517cd415c903ecadb47769a2bd7352e96
WeichenZh <54957828+WeichenZh@users.noreply.github.com>
2019-10-23 19:42:03 -0400

    Update Dijkstra.cpp

--- nameserver/Dijkstra.cpp
+++ nameserver/Dijkstra.cpp
@@ -8,29 +8,29 @@ int dijkstra(int srcpID, int (*e)[20], int *dis, int *book, int nNodes, int nLin
 	int m = nLinks;
 	// int srcpID = 1;
 
-	//初始化dis数组，例如dis[3]=e[1][3],1到3距离为dis[3].
+	// initial dst array
 	for(i=0;i<n;i++)                
 		dis[i]=e[srcpID][i];
 	for(i=0;i<n;i++)
 		book[i]=0;
 	book[srcpID]=1;
-	//Dj斯特拉算法核心语句
-	for(i=0;i<n-1;i++){                        ////大循环，每一步控制1到i的最短路径。
-	    //找到离一号顶点最近的顶点
+	//Dijkstra core lines
+	for(i=0;i<n-1;i++){                        ////outter loop
+	    //find the closest point to the point 0
 		min=INF;
 		for(j=0;j<n;j++)
 		{
-			if(book[j]==0&&dis[j]<min){        //以1号为例，1到2为1,1到3为12，u就是2.
+			if(book[j]==0&&dis[j]<min){       
 				min=dis[j];
 				u=j;
 			}
 		}
-		book[u]=1;//j在内部循环，在外部要使用，用u保存j的值，常见思想。
+		book[u]=1;
 		for(v=0;v<n;v++)
 		{
-			if(e[u][v]<INF)         //e[2][1]=inf,直到e[2][3]为9
+			if(e[u][v]<INF)         
 			{
-				if(dis[v]>dis[u]+e[u][v])     //dis[2]为1，dis[3]为12，dis[2]+e[2][3]=10,替换。
+				if(dis[v]>dis[u]+e[u][v])     
 					dis[v]=dis[u]+e[u][v];
 			}
 		}
