f6b44b4e5a4a7215585c8966a3a6d52ff1435272
WeichenZh <54957828+WeichenZh@users.noreply.github.com>
2019-10-25 00:42:21 -0400

    Update Geo_loader.cpp

--- nameserver/Geo_loader.cpp
+++ nameserver/Geo_loader.cpp
@@ -39,8 +39,8 @@ int Geo_loader(int argc, char const *argv[])
 
     ofstream log;
     log.open(log_path, ios::trunc);
-    if(!log)
-        error("Error: no log file!\n");
+    // if(!log)
+    //     error("Error: no log file!\n");
     log.close();
 
     /* Decoding query package config */
@@ -48,16 +48,16 @@ int Geo_loader(int argc, char const *argv[])
     char qDomainName[100] = {0};
     char rData[100] = {0};
     char rCode = 0;
-    int DNSHeader_size, DNSQuestion_size;
-    int DNSAHeader_size, DNSARecord_size;
-    DNSHeader queryHeader,answerHeader;
+    DNSHeader queryHeader;
     DNSQuestion queryQuestion;
+    DNSHeader answerHeader;
     DNSRecord answerRecord;
 
     /**** socket part ****/
     int sockfd, n_recv, portno; 
     const int optval = 1;
-    char buffer[MAXLINE], srcIPaddr[20]; 
+    char buffer[MAXLINE]; 
+    char srcIPaddr[20];
     struct sockaddr_in servaddr, cliaddr; 
     socklen_t len = sizeof(cliaddr);
 
@@ -65,10 +65,11 @@ int Geo_loader(int argc, char const *argv[])
     if ( (sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0 ) { 
         perror("socket creation failed"); 
         exit(EXIT_FAILURE); 
-    }
+    } 
     
     if (setsockopt(sockfd, SOL_SOCKET, SO_REUSEADDR, &optval, sizeof(int)) < 0)
         error("setsockopt(SO_REUSEADDR) failed");
+
     memset(&servaddr, 0, sizeof(servaddr)); 
     memset(&cliaddr, 0, sizeof(cliaddr)); 
       
@@ -79,13 +80,13 @@ int Geo_loader(int argc, char const *argv[])
     servaddr.sin_port = htons(portno); 
       
     // Bind the socket with the server address 
-    if ( bind(sockfd, (const struct sockaddr *)&servaddr,  sizeof(servaddr)) < 0 ) 
+    if ( bind(sockfd, (const struct sockaddr *)&servaddr,  
+            sizeof(servaddr)) < 0 ) 
     { 
         perror("bind failed"); 
         exit(EXIT_FAILURE); 
     } 
     listen(sockfd, 5);
-
     for(;;)
     {
         int newsockfd = accept(sockfd, (struct sockaddr *)&cliaddr, &len);
@@ -94,35 +95,44 @@ int Geo_loader(int argc, char const *argv[])
 
         //get client ip address
         strcpy(srcIPaddr, inet_ntoa(cliaddr.sin_addr));
-
         // receive size of DNS header
-        memset(buffer, 0, MAXLINE);
+        int DNSHeader_size;
         n_recv = recv(newsockfd, (char *)buffer, sizeof(DNSHeader_size),  0); 
         if (n_recv<0)
             error("ERROR reading from socket");
+
         memcpy(&DNSHeader_size, (BYTE *)buffer, sizeof(DNSHeader_size));
         DNSHeader_size = ntohl(DNSHeader_size);
-        if (DNSQuestion_size <= 0)
-            error("Error: DNS question size must be greater than 0\n");
+
 
         // receive DNS header
         memset(buffer, 0, MAXLINE);
         n_recv = recv(newsockfd, (char *)buffer, DNSHeader_size, 0);
-        if (n_recv < 0) 
+        if (n_recv < 0)
             error("ERROR reading from socket");
-        queryHeader = DNSHeader::decode(string(buffer, DNSHeader_size));
+        queryHeader = DNSHeader::decode(string(buffer));
 
+        // Decoding Header
         // check the AA flag
         if(queryHeader.AA != 0x0)
+        {
             error("Error: received package is not query package\n");
+        }
+
+        // Decoding question
+        /******************************************************/
+        /* TUDO: should I return error if QTYPE or QCLASS is not equal to 1? */
+        /******************************************************/
         
         // receive DNS Question size
+        int DNSQuestion_size;
         memset(buffer, 0, MAXLINE);
         n_recv = recv(newsockfd, (char *)buffer, sizeof(DNSQuestion_size),  0); 
         if (n_recv<0)
             error("ERROR reading from socket");
         memcpy(&DNSQuestion_size, (BYTE *)buffer, sizeof(DNSQuestion_size));
         DNSQuestion_size = ntohl(DNSQuestion_size);
+        // cout << "DNS Question size: " << DNSQuestion_size << endl;
         if (DNSQuestion_size < 0)
             error("Error: DNS question size must be greater than 0\n");
 
@@ -131,9 +141,9 @@ int Geo_loader(int argc, char const *argv[])
         n_recv = recv(newsockfd, (char *)buffer, DNSQuestion_size, 0);
         if (n_recv<0)
             error("ERROR reading from socket");
-        queryQuestion = DNSQuestion::decode(string(buffer, DNSQuestion_size));
+        queryQuestion = DNSQuestion::decode(string(buffer));
 
-        // geometry balance loader
+        // Seach for corresponding ip address
         if (!strcmp(queryQuestion.QNAME, domainName))
         {
             rCode = 0x0;
@@ -145,6 +155,7 @@ int Geo_loader(int argc, char const *argv[])
             memset(rData, 0, 100);
         }
 
+        // Encoding Answer package
         // Encoding package Header
         memset(&answerHeader, 0, sizeof(answerHeader));
         answerHeader.ID = queryHeader.ID;  // ID
@@ -161,24 +172,23 @@ int Geo_loader(int argc, char const *argv[])
         // Encoding package record
         memset(&answerRecord, 0, sizeof(answerRecord));
         memcpy(answerRecord.NAME, queryQuestion.QNAME, 100);
-        memcpy(answerRecord.RDATA, rData, 100); 
         answerRecord.TYPE = htons(0x1);
         answerRecord.CLASS = htons(0x1);
         answerRecord.TTL = htons(0x0);
         answerRecord.RDLENGTH = htons(sizeof(answerHeader)+sizeof(answerRecord));
+        memcpy(answerRecord.RDATA, rData, 100); 
 
         // send DNS packet
         string sAnswerHeader = DNSHeader::encode(answerHeader);
         string sAnswerRecord = DNSRecord::encode(answerRecord);
-        DNSAHeader_size = htonl(sAnswerHeader.length());
-        DNSARecord_size = htonl(sAnswerRecord.length());
+        int DNSAHeader_size = htonl(sAnswerHeader.length());
+        int DNSARecord_size = htonl(sAnswerRecord.length());
         int nSent = 0;
 
         nSent = send(newsockfd,
             (char *)&DNSAHeader_size,
             sizeof(DNSAHeader_size),
             0);
-
         memset(buffer, 0, MAXLINE);
         memcpy(buffer, sAnswerHeader.c_str(), ntohl(DNSAHeader_size));
         nSent = send(newsockfd, 
@@ -190,14 +200,13 @@ int Geo_loader(int argc, char const *argv[])
             (char *)&DNSARecord_size,
             sizeof(DNSARecord_size),
             0);
-
         memset(buffer, 0, MAXLINE);
         memcpy(buffer, sAnswerRecord.c_str(), ntohl(DNSARecord_size));
         nSent = send(newsockfd,
             (char *)buffer,
             ntohl(DNSARecord_size),
             0);
-
+        
         // logging
         log.open(log_path, ios::app);
         log << srcIPaddr << " " << qDomainName << " " << rData << "\n";
@@ -294,6 +303,23 @@ int nearest_server_addr(const char *servers, char *query_ip, char *nearest_serve
 
 	dijkstra(IPtoID[object_ip], edge, dst, book, nNodes, nLinks);
 
+	// int serverID = distance(dst, min_element(&dst[serverIDs[0]], &dst[serverIDs[0]]+num_server));
+    // // print out the result of records
+    // cout << endl;
+    // cout << typeid(serverIDs[0]).name() << endl;
+    // cout << " distance: ";
+    // for (int i =0;i<num_server;i++)
+    //     cout << dst[serverIDs[i]] << " ";
+    // cout << endl;
+    // cout << " serverID: ";
+    // for (int i=0;i<num_server;i++)
+    //     cout << serverIDs[i] << " ";
+    // cout << endl;
+    // cout << " client ID: ";
+    // for (int i=0;i <num_client; i++)
+    //     cout << clientIDs[i] << " ";
+    // cout << endl;
+
     // find the closest server IP
     float min_dis = 100000.0;
     int clst_serverID=-1;
@@ -305,7 +331,8 @@ int nearest_server_addr(const char *servers, char *query_ip, char *nearest_serve
             clst_serverID = serverIDs[i];
         }
     }
-
+    // cout << " closest server ID: ";
+    // cout << clst_serverID << endl;
 	string serverIP = IDtoIP[clst_serverID];
 	if (!serverIP.size())
 		cout << "Error: invalid address." <<endl;
@@ -314,4 +341,3 @@ int nearest_server_addr(const char *servers, char *query_ip, char *nearest_serve
     records.close();
 	return 0;
 }
-
